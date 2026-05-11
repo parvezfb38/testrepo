2e1aef4c1db0954dd17d7c2366d272e10b5fd87f
parvezfb38 <parvezfb38@gmail.com>
2026-05-11 16:29:41 -0400

    updated webhook url
    Added comment for updated web hook URL.

--- database.py
+++ database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./ecommerce.db"
+DATABASE_URL = "sqlite:///./ecommerce.db"  
+# updated web hook url
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
