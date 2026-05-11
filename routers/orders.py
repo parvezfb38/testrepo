f2ae10f0bf3c1c8a0afe15128d83d59360606d00
parvezfb38 <parvezfb38@gmail.com>
2026-05-11 16:42:43 -0400

    Update comments in orders.py

--- routers/orders.py
+++ routers/orders.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from database import get_db
 import models, schemas
 
-# order routers - upated teh comment
+# order routers - upated the comments
 
 router = APIRouter(prefix="/orders", tags=["Orders"])
 
