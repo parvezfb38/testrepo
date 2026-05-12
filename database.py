dcd75fc84d1839faade1b38c32a1e2e19f6b9513
parvezfb38 <parvezfb38@gmail.com>
2026-05-11 22:09:29 -0400

    test1
    Removed 'trigger lambda' from the comment for clarity.

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "sqlite:///./ecommerce.db"  
-# updated web hook url for github trigger lambda
+# updated web hook url
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
