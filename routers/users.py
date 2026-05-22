932906b660c8d20e1ae9ae12b62b2ef8e067f1fc
parvezfb38 <parvezfb38@gmail.com>
2026-05-22 18:28:37 -0400

    Refactor user router code for clarity

--- routers/users.py
+++ routers/users.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from database import get_db
 import models, schemas
-
+# test?
 router = APIRouter(prefix="/users", tags=["Users"])
 
 @router.post("/", response_model=schemas.UserOut, status_code=201)
