92a52ddef86090ab0901b889362cec8592eed4a1
parvezfb38 <parvezfb38@gmail.com>
2026-05-31 15:01:15 -0400

    Update users.py

--- routers/users.py
+++ routers/users.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from database import get_db
 import models, schemas
-# test? testing testagainruntimesworker testwith regression agent  regressiontest with dynamobd
+# test? testing testagainruntimesworker testwith regression agent  regressiontest with dynamobd matrics fetch
 router = APIRouter(prefix="/users", tags=["Users"]) 
 
 @router.post("/", response_model=schemas.UserOut, status_code=201)
