7154c77a037d5d591c5a31feb6bba4f3b76ca205
parvezfb38 <parvezfb38@gmail.com>
2026-05-31 15:03:07 -0400

    Update orders.py

--- routers/orders.py
+++ routers/orders.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from database import get_db
 import models, schemas
 
-# order routers metrics fetch test
+# order routers metrics fetch test againtest
 
 router = APIRouter(prefix="/orders", tags=["Orders"])
 
