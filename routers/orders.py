ab4a5b00d2b588a68fc7626217f120dc090d793e
parvezfb38 <parvezfb38@gmail.com>
2026-06-01 14:32:17 -0400

    Refactor order router with updated comments

--- routers/orders.py
+++ routers/orders.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from database import get_db
 import models, schemas
 
-# order routers metrics fetch test againtest dynamoDB test regression debug regression test fetch metrics - test 
+# order routers metrics fetch test againtest dynamoDB test regression debug regression test fetch metrics - testing for token
 
 router = APIRouter(prefix="/orders", tags=["Orders"])
 
